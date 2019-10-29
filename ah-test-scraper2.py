3e62289a6d5a1258865b3d8a798b945e4b459601
Marten van Dijk <marten@huygens.local>
2019-10-29 13:31:02 +0100

    database test

--- ah-test-scraper2.py
+++ ah-test-scraper2.py
@@ -1,14 +1,16 @@
 import requests
 from bs4 import BeautifulSoup
 import json
-import lxml
-import re
+import mysql.connector as mariadb
 
+
+mariadb_connection = mariadb.connect(user="s4dpython", password="s4dpython", database="producten")
 ah_url = "https://www.ah.nl/producten/product"
+cursor = mariadb_connection.cursor()
 
 prod_dict = {}
 
-for i in range(435427,436427):
+for i in range(435427,435437):
     product_suffix = "/wi" + str(i)
     temp_url = ah_url+product_suffix
     r = requests.get(temp_url)
@@ -17,4 +19,14 @@ for i in range(435427,436427):
         element = soup.find("script", type="application/ld+json").text
         element = json.loads(element)
         if 'offers' in element:
-            print(element['offers']['price'])
+            productnaam = element['name']
+            prijs = element['offers']['price']
+            product_url = element['url']
+            gewicht = element['weight']
+            imagelink = element['image']
+            cursor.execute("INSERT INTO albert_heijn (productnaam, prijs, product_url, gewicht, imagelink) VALUES (%s, %s, %s, %s, %s)",(productnaam, prijs, product_url, gewicht, imagelink))
+            mariadb_connection.commit()
+            print(element['name'], element['offers']['price'])
+
+
+
