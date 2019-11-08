1b082ea62d99b560620a296da4921b79b5251aa1
Marten van Dijk <marten@huygens.local>
2019-11-08 18:08:16 +0100

    added extra rule for docker

--- docker_images/firewall/iptables_rules.sh
+++ docker_images/firewall/iptables_rules.sh
@@ -4,6 +4,7 @@ iptables -I DOCKER -p tcp --dport 3306 -j DROP
 iptables -I DOCKER -p tcp --dport 3306 -d 127.0.0.1/8 -j ACCEPT
 iptables -I DOCKER -p tcp --dport 3306 -d 172.17.0.1/16 -s 127.0.0.1/8 -j ACCEPT
 iptables -I DOCKER -p tcp --dport 3306 -d 172.17.0.1/16 -s 213.190.22.172/32 -j ACCEPT
+iptables -I DOCKER -p tcp --dport 3306 -d 213.190.22.172/32 -s 172.17.0.1/16 -j ACCEPT
 
 iptables -I INPUT -p tcp --dport 3306 -j DROP
 iptables -I INPUT -p tcp --dport 3306 -d 127.0.0.1/8 -j ACCEPT
