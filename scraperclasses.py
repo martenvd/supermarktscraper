03a2b93d40556464731947169e39fd42ee2134da
L4mpje <baslubbers91@gmail.com>
2019-11-11 10:30:40 +0100

    Scraperclasses aangepast

--- scraperclasses.py
+++ scraperclasses.py
@@ -5,42 +5,89 @@ import mysql.connector as mariadb
 import time
 import re
 
+# TODO: alles hieronder
+# De Coop Scraper werkt niet naar behoren.
+# Naamgeving Albert_Heijn categorielijsten aanpassen naar de standaard.
+# Testen na de aanpassingen.
 
-### MAIN CLASS ###
+### Static Settings Class ###
+class Settings:
+    database_user = "s4dpython"
+    database_password = "s4dpython"
+    database_db = "producten"
+    database_port = 3307
+    database_host = "213.190.22.172"
 
-class Main_Scraper:
-    def __init__(self, name, url):
+### Product Class ###
+class Product:
+    def __init__(self, supermarket, name, price, quantity, url, image):
+        self.supermarket = supermarket
         self.name = name
+        self.price = price
+        self.quantity = quantity
         self.url = url
-        self.headers = {}
-        self.mariadb_connection = mariadb.connect(user="s4dpython", password="s4dpython", database="producten")
+        self.image = image
+
+    def __repr__(self):
+        return "Product Class for: {} from supermarket: {}.".format(self.name, self.supermarket)
+
+### Static Database Class ###
+class Database:
+    def __init__(self, supermarket):
+        self.supermarket = supermarket
+        self.mariadb_connection = mariadb.connect(user=Settings.database_user, password=Settings.database_password,
+                                                  database=Settings.database_db, host=Settings.database_host,
+                                                  port=Settings.database_port)
         self.cursor = self.mariadb_connection.cursor()
 
     def __repr__(self):
-        return "Scraper class voor : " + self.name
+        return "Database Class for: {}".format(self.supermarket)
 
-    def soup(self, url, soupOrNot):
-        r = requests.get(url, headers=self.headers)
-        soup = BeautifulSoup(r.text, 'lxml')
+    # TODO: advies vragen over de product.supermarket vs self.supermarket.
+    #  Welke optie is beter? laten afhangen van het product of van de Class instance?
+    def write_product(self, product):
+        try:
+            self.cursor.execute("INSERT INTO {} (productnaam, prijs, product_url, hoeveelheid, imagelink) "
+                                "VALUES (%s, %s, %s, %s, %s)".format(product.supermarket),
+                                (product.name, product.price, product.url, product.quantity, product.image))
+            self.mariadb_connection.commit()
+        except mariadb.Error as error:
+            print(error)
+
+
+### PARENT SCRAPER CLASS ###
+class Scraper:
+    def __init__(self, name, url, url_suffix, headers):
+        self.name = name
+        self.url = url
+        self.url_suffix = url_suffix
+        self.headers = headers
+        self.table_name = self.name.lower()
+        self.database = Database(self.table_name)
+        self.products = []
+        self.categories_url = []
+
+    def __repr__(self):
+        return "Scraper Class for: {}".format(self.name)
+
+    def soup(self, url):
+        response = self.response(url)
+        soup = BeautifulSoup(response.text, 'lxml')
         soup.encode('UTF-8')
-        if soupOrNot:
-            return soup
-        else:
-            return r
+        return soup
 
-
+    def response(self, url):
+        return requests.get(url, headers=self.headers)
 
 
 ### JUMBO ###
+class Jumbo_scraper(Scraper):
+    def __init__(self):
+        super().__init__(name="Jumbo", url="https://www.jumbo.com", url_suffix="/producten/?PageNumber=",
+                         headers={"User-Agent": "Mozilla/5.0 (X11; Linux x86_64; rv:60.0) Gecko/20100101 Firefox/60.0"})
+        #self.fetch_all_products()
 
-class Jumbo_Product_Scraper(Main_Scraper):
-    def __init__(self, name, url):
-        super().__init__(name, url)
-        self.products = []
-        self.product_suffix = "/producten/?PageNumber="
-        self.headers = {"User-Agent": "Mozilla/5.0 (X11; Linux x86_64; rv:60.0) Gecko/20100101 Firefox/60.0"}
-
-    def get_jumbo_products(self):
+    def fetch_all_products(self):
         all_products_reached = False
         all_pages_reached = False
         product_index = 0
@@ -48,33 +95,31 @@ class Jumbo_Product_Scraper(Main_Scraper):
 
         while not all_pages_reached:
             # Delay om meer human te lijken.
-            time.sleep(1)
-            r = self.soup(self.url + self.product_suffix + str(page_index), False)
-            soup = self.soup(self.url + self.product_suffix + str(page_index), True)
-            #r = requests.get(self.url + self.product_suffix + str(page_index), headers=self.headers)
-            #soup = BeautifulSoup(r.text, "lxml")
-            if len(r.text) > 2200:
+            time.sleep(0.2)
+            print("[+] Next page is number: {}".format(page_index))
+            response = self.response(self.url + self.url_suffix + str(page_index))
+            soup = self.soup(self.url + self.url_suffix + str(page_index))
+            if len(response.text) > 2200:
                 while not all_products_reached:
                     try:
                         productnaam = soup.find_all("a", href=True)[product_index].text
                         product_url = soup.find_all("a", href=True)[product_index]['href']
                         prijs = soup.find_all("span", class_="jum-price-format")[product_index].text
                         hoeveelheid = soup.find_all("span", class_="jum-pack-size")[product_index].text
-                        imagelink = re.findall("data-jum-hr-src=(.*?) ", str(soup.find_all("figure")[product_index]))[
-                            0].replace('"', '')
+                        imagelink = re.findall("data-jum-hr-src=(.*?) ",
+                                               str(soup.find_all("figure")[product_index]))[0].replace('"', '')
+
+                        # TODO: wanneer de prijs per kilo/liter is iets verzinnen om te converten.
                         try:
-                            price = int(prijs) / 100
+                            prijs = int(prijs) / 100
                         except:
                             pass
-                        dict_string = {'productnaam': productnaam, 'prijs': prijs, 'product_url': product_url,
-                                       'imagelink': imagelink, 'hoeveelheid': hoeveelheid}
-                        self.products.append(dict_string)
-                        print(self.products)
+
+                        product = Product(self.table_name, productnaam, prijs, hoeveelheid, product_url, imagelink)
+                        self.products.append(product)
+                        self.database.write_product(product)
+
                         product_index += 1
-                        self.cursor.execute(
-                            "INSERT INTO jumbo (productnaam, prijs, product_url, hoeveelheid, imagelink) VALUES (%s, %s, %s, %s, %s)",
-                            (productnaam, prijs, product_url, hoeveelheid, imagelink))
-                        self.mariadb_connection.commit()
                     except:
                         all_products_reached = True
             else:
@@ -83,77 +128,70 @@ class Jumbo_Product_Scraper(Main_Scraper):
             page_index += 1
             all_products_reached = False
 
-
-
-
 ### ALBERT HEIJN ###
-
-class AH_Product_Scraper(Main_Scraper):
-    def __init__(self, name, url):
-        super().__init__(name, url)
-        self.suffix = '/producten'
+class AH_scraper(Scraper):
+    def __init__(self):
+        super().__init__(name="Albert_Heijn", url="https://www.ah.nl", url_suffix="/producten",
+                         headers={"User-Agent": "Mozilla/5.0 (X11; Linux x86_64; rv:60.0) Gecko/20100101 Firefox/60.0"})
         self.categorie_url_list = []
         self.categorie_list = []
         self.producten_url_list = []
+        #self.fetch_all_products()
 
-    def scraper(self):
-        for a in self.soup(self.url + self.suffix, True).find_all('a', href=True):
+    def fetch_all_products(self):
+        for a in self.soup(self.url + self.url_suffix).find_all('a', href=True):
             if "/producten/" in a['href'] and "/merk" not in a['href'] and "/eerder-gekocht" not in a['href']:
                 self.categorie_url_list.append(self.url + a['href'])
-                print(self.url + a['href'])
+                #print(self.url + a['href'])
 
         self.categorie_url_list = list(set(self.categorie_url_list))
 
         for link in self.categorie_url_list:
-            for a in self.soup(link + '?page=2000', True).find_all('a', href=True):
+            for a in self.soup(link + '?page=2000').find_all('a', href=True):
                 if "/producten/product" in a['href']:
                     self.producten_url_list.append(self.url + a['href'])
-                    print(self.url + a['href'])
+                    #print(self.url + a['href'])
 
         self.producten_url_list = list(set(self.producten_url_list))
 
         try:
             for link in self.producten_url_list:
-                element = self.soup(link, True).find("script", type="application/ld+json").text
+                element = self.soup(link).find("script", type="application/ld+json").text
                 element = json.loads(element)
                 if 'offers' in element:
                     productnaam = element['name']
                     prijs = element['offers']['price']
                     product_url = element['url']
-                    gewicht = element['weight']
+                    hoeveelheid = element['weight']
                     imagelink = element['image']
-                    self.cursor.execute(
-                        "INSERT INTO albert_heijn (productnaam, prijs, product_url, hoeveelheid, imagelink) VALUES (%s, %s, %s, %s, %s)",
-                        (productnaam, prijs, product_url, gewicht, imagelink))
-                    self.mariadb_connection.commit()
-                    print(element['name'], element['offers']['price'])
+
+                    product = Product(self.table_name, productnaam, prijs, hoeveelheid, product_url, imagelink)
+                    self.products.append(product)
+                    self.database.write_product(product)
         except:
             pass
 
-
-
 ### ALDI ###
+class Aldi_scraper(Scraper):
+    def __init__(self):
+        super().__init__(name="Aldi", url="https://www.aldi.nl", url_suffix=None,
+                         headers={"User-Agent": "Mozilla/5.0 (X11; Linux x86_64; rv:60.0) Gecko/20100101 Firefox/60.0"})
+        self.fetch_all_categories()
+        #self.fetch_all_products()
 
-class Aldi_Product_Scraper(Main_Scraper):
-    def __init__(self, name, url):
-        super().__init__(name, url)
-        self.categories = self.get_aldi_categories()
-        self.products = []
-
-    def get_aldi_categories(self):
-        temp_list = []
-        for a in self.soup(self.url, True).find_all('a', href=True):
+    def fetch_all_categories(self):
+        for a in self.soup(self.url).find_all('a', href=True):
             if "/onze-producten/" in a['href']:
-                temp_list.append(self.url + a['href'])
-        temp_list = list(set(temp_list))
-        return temp_list
+                # Delay om human te lijken.
+                time.sleep(0.1)
+                self.categories_url.append(self.url + a['href'])
+        self.categories_url = list(set(self.categories_url))
 
-    def get_aldi_product(self):
-        for categorie in self.categories:
-            categorie_url = categorie
-            #Delay om human te lijken.
-            time.sleep(0.5)
-            element = self.soup(categorie_url, True).find_all("script", type="application/ld+json")[-1].text
+    def fetch_all_products(self):
+        for link in self.categories_url:
+            # Delay om human te lijken.
+            time.sleep(0.2)
+            element = self.soup(link).find_all("script", type="application/ld+json")[-1].text
             element = json.loads(element)
             try:
                 num_items = element['numberOfItems']
@@ -162,85 +200,45 @@ class Aldi_Product_Scraper(Main_Scraper):
                     prijs = element['itemListElement'][i]['offers']['price']
                     product_url = element['itemListElement'][i]['url']
                     imagelink = element['itemListElement'][i]['image']
-                    dict_string = {'productnaam': productnaam, 'prijs': prijs, 'product_url': product_url, 'imagelink': imagelink}
-                    self.products.append(dict_string)
-                    self.cursor.execute(
-                        "INSERT INTO aldi (productnaam, prijs, product_url, imagelink) VALUES (%s, %s, %s, %s)",
-                        (productnaam, prijs, product_url, imagelink))
-                    self.mariadb_connection.commit()
-                    #print(productnaam, prijs, product_url, imagelink)
+
+                    # TODO: hoeveelheid ophalen bij Aldi, is voor nu NULL.
+                    # TODO: wanneer de prijs per kilo/liter is iets verzinnen om te converten.
+                    product = Product(self.table_name, productnaam, prijs, "NULL", product_url, imagelink)
+                    self.products.append(product)
+                    print(self.products)
+                    self.database.write_product(product)
             except:
                 pass
 
 
 ### COOP CLASS ####
+class Coop_scraper(Scraper):
+    def __init__(self):
+        super().__init__(name="Coop", url="https://www.coop.nl", url_suffix="/boodschappen/",
+                         headers={"User-Agent": "Mozilla/5.0 (X11; Linux x86_64; rv:60.0) Gecko/20100101 Firefox/60.0"})
+        self.fetch_all_categories()
+        #self.fetch_all_products()
 
-class Coop_Product_Scraper(Main_Scraper):
-    products = []
+    def fetch_all_categories(self):
+        for link in self.soup(self.url + self.url_suffix).find_all('a', href=True):
+            if "/boodschappen/" in link['href']:
+                self.categories_url.append(link['href'])
+        self.categories_url = list(set(self.categories_url))
 
-    def __init__(self, name, url):
-        super().__init__(name, url)
-        self.suffix = '/boodschappen/'
-        self.fetch_all()
-
-    #def parse_page(self, url):
-        #page = requests.get(url)
-        #soup = BeautifulSoup(page.text, 'html.parser')
-        #soup.encode('UTF-8')
-        #return soup
-
-    def fetch_categories(self):
-        categories = []
-        for link in self.soup(self.url + self.suffix, True).find_all('a', href=True):
-            if '/boodschappen/' in link['href']:
-                categories.append(link['href'])
-        categories = list(set(categories))
-        return categories
-
-
-    def fetch_products(self, category_url):
-        page = self.soup(category_url + "?PageSize=99999", True)  # haal pagina met producten in categorie op
-        for article in page.find_all('article'):  # loop over alle producten heen
-            try:
-                img = article.find('img')['data-srcset'].split()[0]  # afbeelding van product
-                data = json.loads(article['data-product'])  # {"id":"3509","price":"1.69","list":"product-category","variant":"1 stuk(s)"}
-                name = article.find('h2')['title']  # naam van product
-                product_url = article.find('a')['href']
-                hoeveelheid = data['variant']
-                prijs = float(data['price'])
-                product = {
-                    'id': data['id'], # heeft ah dit ook?
-                    'naam': name,
-                    'prijs': float(data['price']),
-                    'product_url': product_url,
-                    'hoeveelheid': data['variant'],
-                    'image_url': img
-                }
-                self.products.append(product)
+    def fetch_all_products(self):
+        for link in self.categories_url:
+            page = self.soup(link + "?PageSize=99999")
+            for article in page.find_all('article'):
                 try:
-                    self.cursor.execute(
-                        "INSERT INTO coop (productnaam, prijs, product_url, hoeveelheid, imagelink) VALUES (%s, %s, %s, %s, %s)",
-                        (name, prijs, product_url, hoeveelheid, img))
-                    self.mariadb_connection.commit()
-                except mariadb.Error as err:
-                    print(err)
-            except:
-                pass
+                    img = article.find('img')['data-srcset'].split()[0]
+                    data = json.loads(article['data-product'])
+                    name = article.find('h2')['title']
+                    product_url = article.find('a')['href']
+                    hoeveelheid = data['variant']
+                    prijs = float(data['price'])
 
-
-    def fetch_all(self):
-        categories = self.fetch_categories() # haal cat pagina op en parse alle categorieen
-        for cat in categories:
-            self.fetch_products(cat) # haal alle producten van cateegorieen op
-
-
-
-#jumbo = Jumbo_Product_Scraper("Jumbo", "https://www.jumbo.com/")
-#albert_heijn = AH_Product_Scraper("Albert Heijn", "https://www.ah.nl")
-#aldi = Aldi_Product_Scraper("Aldi", "https://www.aldi.nl/")
-#coop = Coop_Product_Scraper("COOP", "https://www.coop.nl")
-
-#jumbo.get_jumbo_products()
-#albert_heijn.scraper()
-#aldi.get_aldi_product()
-#print(coop.products)
+                    product = Product(self.table_name, name, prijs, hoeveelheid, product_url, img)
+                    self.products.append(product)
+                    self.database.write_product(product)
+                except:
+                    pass
