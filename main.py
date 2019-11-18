5359d9feb90714c456736a5b811ef8b7b2d70bf6
L4mpje <baslubbers91@gmail.com>
2019-11-18 15:09:41 +0100

    Argument support toegevoegd voor main.py

--- main.py
+++ main.py
@@ -1,12 +1,33 @@
 from scraperclasses import *
 import threading
+import sys
 
 aldi = Aldi_scraper()
 jumbo = Jumbo_scraper()
 coop = Coop_scraper()
 albert_heijn = AH_scraper()
 
-supermarkets = [aldi, jumbo, coop, albert_heijn]
+supermarkets = []
+
+#Zonder argumenten worden alle supermarkten gescraped.
+if len(sys.argv) < 2:
+    supermarkets = [aldi, jumbo, coop, albert_heijn]
+else:
+    for argument in sys.argv:
+        supermarkets.append(argument)
+del supermarkets[0]
+
+for index in range(len(supermarkets)):
+    if supermarkets[index] == "aldi":
+        supermarkets[index] = aldi
+    elif supermarkets[index] == "jumbo":
+        supermarkets[index] = jumbo
+    elif supermarkets[index] == "albert_heijn":
+        supermarkets[index] = albert_heijn
+    elif supermarkets[index] == "coop":
+        supermarkets[index] = coop
+    else:
+        print("[-] Invalid supermarket in arguments: {}".format(supermarkets[index]))
 
 def start_scraper_thread(supermarket):
     print("[+] Starting scraper thread for {}".format(supermarket))
